15505eadde3aea941f5c1439d6f2d0380ef138c8
lzh <2115883273@qq.com>
2024-04-12 11:33:30 +0800

    sonkeflake错误处理优雅化

--- pkg/snowflake/snowflake.go
+++ pkg/snowflake/snowflake.go
@@ -10,10 +10,8 @@ var node *sf.Node
 
 func Init(startTime string, machineId int64) (err error) {
 	var st time.Time
-	st, err = time.Parse("2006-01-02", startTime)
-	if err != nil {
-		fmt.Printf("time.Parse() failed,err:%v", err)
-		return err
+	if st, err = time.Parse("2006-01-02", startTime); err != nil {
+		return fmt.Errorf("time.Parse(\"2006-01-02\", startTime) failed:%v", err)
 	}
 	sf.Epoch = st.UnixNano() / 1000000
 	node, err = sf.NewNode(machineId)
