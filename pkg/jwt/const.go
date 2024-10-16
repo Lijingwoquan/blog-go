6fde0f33dc41d22a6aadd4daddbf73bcf9510612
lzh <2115883273@qq.com>
2024-10-16 10:55:18 +0800

    重构jwt认证

--- pkg/jwt/const.go
+++ pkg/jwt/const.go
@@ -1,5 +1,1 @@
 package jwt
-
-const (
-	mySecret = "老子就是喜欢看美女怎么滴"
-)
--- pkg/jwt/jwt.go
+++ pkg/jwt/jwt.go
@@ -3,50 +3,55 @@ package jwt
 import (
 	"blog/models"
 	"errors"
-	"github.com/dgrijalva/jwt-go"
-	"github.com/spf13/viper"
 	"time"
+
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/spf13/viper"
 )
 
-// MyClaims 自定义声明结构体并内嵌jwt.StandardClaims
-// jwt包自带的jwt.StandardClaims只包含了官方字段
-// 我们这里需要额外记录username和userID字段，所以要自定义结构体
-// 如果想要保存更多信息，都可以添加到这个结构体中
+// MyClaims 自定义声明结构体并内嵌 jwt.RegisteredClaims
 type MyClaims struct {
 	UserID int64 `json:"user_id"`
-	jwt.StandardClaims
+	jwt.RegisteredClaims
+}
+
+var mySecret []byte
+
+func init() {
+	// 从配置中读取 secret，确保在使用前已正确设置
+	mySecret = []byte(viper.GetString("auth.jwt_secret"))
 }
 
 // GenToken 生成JWT
 func GenToken(u *models.User) (string, error) {
-	// 创建一个我们自己的声明的数据
-	c := MyClaims{
-		u.UserID,
-		jwt.StandardClaims{
-			ExpiresAt: time.Now().Add( //当下时间加上一周时间
-				time.Duration(viper.GetInt("auth.jwt_expire")) * time.Hour).Unix(), // 过期时间
-			Issuer: "liuzihao-blog", // 签发人
+	expirationTime := time.Now().Add(time.Duration(viper.GetInt("auth.jwt_expire")) * time.Hour)
+
+	claims := &MyClaims{
+		UserID: u.UserID,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+			NotBefore: jwt.NewNumericDate(time.Now()),
+			Issuer:    "liuzihao-blog",
 		},
 	}
-	// 使用指定的签名方法创建签名对象 --> 得到一个结构体
-	token := jwt.NewWithClaims(jwt.SigningMethodHS256, c)
 
-	// 使用指定的secret签名并获得完整的编码后的字符串token
-	return token.SignedString([]byte(mySecret))
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	return token.SignedString(mySecret)
 }
 
 // ParseToken 解析JWT
 func ParseToken(tokenString string) (*MyClaims, error) {
-	// 解析token
-	var mc = new(MyClaims)
-	token, err := jwt.ParseWithClaims(tokenString, mc, func(token *jwt.Token) (i interface{}, err error) {
-		return []byte(mySecret), nil
+	token, err := jwt.ParseWithClaims(tokenString, &MyClaims{}, func(token *jwt.Token) (interface{}, error) {
+		return mySecret, nil
 	})
+
 	if err != nil {
 		return nil, err
 	}
-	if token.Valid { // 校验token
-		return mc, nil
+
+	if claims, ok := token.Claims.(*MyClaims); ok && token.Valid {
+		return claims, nil
 	}
 
 	return nil, errors.New("invalid token")
